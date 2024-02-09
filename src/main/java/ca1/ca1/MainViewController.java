7b2ae30f03e2f4a783bb9c50b8bcbc832213888b
2000krysztof <2000krzysztof@gmail.com>
2024-02-09 15:24:19 +0000

    Add the Image loading functionality. Make the image of size 300 x 300.

--- src/main/java/ca1/ca1/MainViewController.java
+++ src/main/java/ca1/ca1/MainViewController.java
@@ -1,12 +1,45 @@
 package ca1.ca1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainViewController {
-    @FXML
-    protected void selectImageFromFile(){
 
+    FileChooser fileChooser;
+
+    @FXML
+    ImageView imageView;
+
+    @FXML
+    ComboBox<Image> imageSelectComboBox;
+    Image selectedImage;
+    @FXML
+    protected void initialize(){
+        fileChooser = new FileChooser();
+        imageSelectComboBox.setOnAction((event)->{
+            selectedImage = imageSelectComboBox.getSelectionModel().getSelectedItem();
+            imageView.setImage(selectedImage);
+        });
+    }
+    @FXML
+    protected void addImageFromFile(){
+        Stage stage = new Stage();
+        File file = fileChooser.showOpenDialog(stage);
+        Image image = new Image(file.toURI().toString()){
+            @Override
+            public String toString() {
+               return file.getName();
+            }
+        };
+        imageSelectComboBox.getItems().add(image);
+        imageSelectComboBox.getSelectionModel().select(
+                imageSelectComboBox.getSelectionModel().getSelectedIndex()+1);
     }
 
     @FXML
